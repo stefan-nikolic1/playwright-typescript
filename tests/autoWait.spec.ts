a48a4be0ef08d4efdacf8c63778cb833a30749db
Stefan Nikolic <stefannikolic@Jovanas-MacBook-Air.local>
2026-07-24 18:58:40 +0200

    first week

---
+++ tests/autoWait.spec.ts
@@ -0,0 +1,44 @@
+import {test, expect} from '@playwright/test'
+
+test.beforeEach(async({page}, testInfo) => {
+    await page.goto('http://uitestingplayground.com/ajax')
+    await page.getByText('Button Triggering AJAX Request').click()
+    testInfo.setTimeout(testInfo.timeout + 50000)
+})
+
+test('auto waiting', async({page}) => {
+    const successButton = page.locator('.bg-success')
+
+    // await successButton.click()
+
+    // const text = await successButton.textContent()
+    // await successButton.waitFor({state: "attached"})
+    // const text = await successButton.allTextContents()
+
+    // expect(text).toContain('Data loaded with AJAX get request.')
+
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.', {timeout: 20000})
+})
+
+test('alternative waits', async({page}) => {
+    const successButton = page.locator('.bg-success')
+
+    //___ wait for element
+    // await page.waitForSelector('.bg-success')
+
+    //___ wait for particular response
+    // await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+
+    //___ wait for network calls to be completed ('NOT RECOMMENDED')
+    await page.waitForLoadState('networkidle')
+
+    const text = await successButton.allTextContents()
+    expect(text).toContain('Data loaded with AJAX get request.')
+})
+
+test('timeouts', async ({page}) => {
+    // test.setTimeout(10000)
+    test.slow()
+    const successButton = page.locator('.bg-success')
+    await successButton.click()
+})
