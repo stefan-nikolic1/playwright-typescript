93cf74c2fd4b6937dcca83036c6a24fd8e8482c7
Stefan Nikolic <stefannikolic@Jovanas-MacBook-Air.local>
2026-07-28 13:16:51 +0200

    feat: add page object model and playwright learning examples

---
+++ tests/usePageObjects.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test'
+import { PageManager } from '../page-objects/pageManager'
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+})
+
+test('navigate to form page', async({page}) => {
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.navigateTo().datepickerPage()
+    await pm.navigateTo().smartTablePage()
+    await pm.navigateTo().toastrPage()
+    await pm.navigateTo().tooltipPage()
+})
+
+test('parametrized methods', async({page}) => {
+    const pm = new PageManager(page)
+    await pm.navigateTo().formLayoutsPage()
+    await pm.onFormLayoutsPage().submitUsingTheGrigdFormWithCredentialsAndSelectOption('tesdt@test.com', 'Welcome1', 'Option 2')
+    await pm.onFormLayoutsPage().sumbitInlineFormWithNameEmailAndCheckbox('John Smith', 'John@test.com', false)
+    await pm.navigateTo().datepickerPage()
+    await pm.onDatepickerPage().selectCommonDatePickerDateFromToday(10)
+    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(6, 10)
+})
